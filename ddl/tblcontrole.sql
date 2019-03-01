929eba26b9d6d88c41d31daad21e9e20a8ccdd3f
Heloa Silva <hsilva25@its.jnj.com>
2019-02-28 22:29:12 -0300

    ddl remove tbl status, update ddl controle

--- ddl/tblcontrole.sql
+++ ddl/tblcontrole.sql
@@ -7,17 +7,19 @@ Atualização:
 Data		Responsável			Descrição	
 31/12/2018	Heloa Silva			Primeira versão
 28/01/2019	Heloa Silva			Trigramação
+28/02/2019  Heloa Silva			Update the field stts_id to status, 
+								delete the table tblstatus
+								
 ----------------------------------------------'
 
 CREATE TABLE controle (
     ctrl_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     tvdd_id INTEGER,
     usu_id INTEGER,
-    stts_id INTEGER,
+    status VARCHAR(30),
     ctrl_descricao VARCHAR(150),
     ctrl_date DATETIME
 );
 
 ALTER TABLE controle ADD FOREIGN KEY (tvdd_id) REFERENCES atividade(tvdd_id);
 ALTER TABLE controle ADD FOREIGN KEY (usu_id) REFERENCES usuario(usu_id);
-ALTER TABLE controle ADD FOREIGN KEY (stts_id) REFERENCES status(stts_id);
