495ee1962a4270862c833bbaab07dde5218fa2a8
Heloa Silva <hsilva25@its.jnj.com>
2019-03-13 17:07:25 -0300

    add styling almost done

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -18,25 +18,29 @@ import { coerceNumberProperty } from '@angular/cdk/coercion';
 export class AddComponent{
   atividade: Atividade;
   gravidade  = 1;
+  gravidadeLabel ='Sem gravidade';
   urgencia = 1;
+  urgenciaLabel ='Pode esperar';
   tendencia = 1;
-  score : number;
-  classificacao : number;
-
-  valueChanged(e) {
-    console.log('e', e);
-  }
+  tendenciaLabel ='Não irá mudar';
+  score = 1;
+  classificacao = 3;
+  classificacaoLabel = 'When possible';
 
   handleScore(){
     this.score = this.gravidade*this.urgencia*this.tendencia;
+
     if (this.score >= 48 && this.score <=128){
       this.classificacao = 1;
+      this.classificacaoLabel = 'Get it done';
     }
     if (this.score >=27 && this.score<=47){
       this.classificacao = 2;
+      this.classificacaoLabel = 'Delegate or Plan';
     }
     if(this.score < 27) {
       this.classificacao = 3;
+      this.classificacaoLabel = 'When possible';
     }
   }
   add(formInfo: any): void {
@@ -55,10 +59,64 @@ export class AddComponent{
     this.atividadeService.save(newAtividade)
     .subscribe();
     alert("Atividade criada com sucesso!");
+
   }
 
   constructor(private atividadeService: AtividadeService) { }
 
+  HandleGravidadeLabel (){
+    if (this.gravidade==1){
+      this.gravidadeLabel='Sem gravidade';
+    }
+    if (this.gravidade==2){
+      this.gravidadeLabel='Pouco grave';
+    }
+    if (this.gravidade==3){
+      this.gravidadeLabel='Grave';
+    }
+    if (this.gravidade==4){
+      this.gravidadeLabel='Muito grave';
+    }
+    if (this.gravidade==5){
+      this.gravidadeLabel='Extremamente grave';
+    }
+  }
+
+  HandleUrgenciaLabel (){
+    if (this.urgencia==1){
+      this.urgenciaLabel='Pode esperar';
+    }
+    if (this.urgencia==2){
+      this.urgenciaLabel='Pouco urgente';
+    }
+    if (this.urgencia==3){
+      this.urgenciaLabel='Urgente, merece atenção no curto prazo';
+    }
+    if (this.urgencia==4){
+      this.urgenciaLabel='Muito urgente';
+    }
+    if (this.urgencia==5){
+      this.urgenciaLabel='Necessidade de ação imediata';
+    }
+  }
+
+  HandleTendenciaLabel (){
+    if (this.tendencia==1){
+      this.tendenciaLabel='Não irá mudar';
+    }
+    if (this.tendencia==2){
+      this.tendenciaLabel='Irá piorar a longo prazo';
+    }
+    if (this.tendencia==3){
+      this.tendenciaLabel='Irá piorar a médio prazo';
+    }
+    if (this.tendencia==4){
+      this.tendenciaLabel='Irá piorar a curto prazo';
+    }
+    if (this.tendencia==5){
+      this.tendenciaLabel='Irá piorar rapidamente';
+    }
+  }
 
 
 }
