f6c7ffa6c53ee84860b005ab84fe4de09bcb7651
heloavfsilva <heloavfsilva@gmail.com>
2019-05-11 12:25:15 -0300

    bug fix da controle e tamanho dos boxes

--- ddl/tblcontrole.sql
+++ ddl/tblcontrole.sql
@@ -1,4 +1,4 @@
-'----------------------------------------------
+﻿'----------------------------------------------
 Criado por: Heloa Silva
 Data: 31/12/2018
 Descrição: Criação da tabela Controle 
@@ -7,7 +7,7 @@ Atualização:
 Data		Responsável			Descrição	
 31/12/2018	Heloa Silva			Primeira versão
 28/01/2019	Heloa Silva			Trigramação
-28/02/2019  Heloa Silva			Update the field stts_id to status, 
+28/02/2019  	Heloa Silva			Update the field stts_id to status, 
 								delete the table tblstatus
 12/03/2019	Heloa Silva			Remove column ctrl_descricao	
 12/03/2019	Heloa Silva			Remove tabela controle				
@@ -17,6 +17,7 @@ CREATE TABLE controle (
     ctrl_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     tvdd_id INTEGER,
     usu_id INTEGER,
+    status VARCHAR(30),
     ctrl_date DATETIME
 );
 
